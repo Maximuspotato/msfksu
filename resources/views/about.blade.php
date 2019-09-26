e9e8afbc3ec7aa556f66f660f270963ae26cb989
Maximuspotato <rickylui28@gmail.com>
2019-09-26 14:33:32 +0300

    a few more edits

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,23 +15,17 @@
         <div class="section">
 	    	<div class="container">
 				<div class="row">
-					{{-- <div class="col-sm-6">
-						<h3>History</h3>
-							<p>KENYA SUPPLY UNIT (KSU) formerly known as Logistical Centre Nairobi (LCN) was established in 2005 following the amalgamations of MSF supply and logistics activities of four sections in Kenya (OCB, OCG, OCP and OCBA). </p>
+					<div class="col-sm-6">
+						{{-- <h3>History</h3>
+						<p>KENYA SUPPLY UNIT (KSU) formerly known as Logistical Centre Nairobi (LCN) was established in 2005 following the amalgamations of MSF supply and logistics activities of four sections in Kenya (OCB, OCG, OCP and OCBA). </p> --}}
 						<h3>About Us</h3>
-						<p>
+						<h4 style="color:#F05E38"><b>COMING SOON!</b></h4><br>
+						{{-- <p>
 							Our ambition is value addition to local and regional clients in Africa and the MSF Supply Chain by focusing on the client needs, development of expertise through unlocking the value of supply chain and logistics.<br><br>
 							We operate in a manner that recognizes and adapts to the increasing complexity of client needs within a rapidly changing environment. KSU strives to make MSF humanitarian responses more efficient (to save costs) and more effective (to save time) through analyzing and optimizing supply chains to enhance procurement, transportation, warehousing and distribution of medical, non-medical and logistical commodities.<br><br>
 							KSU Supports the sustainable development of local labour markets in Kenya in a supply chain setup through the internship program which provides learning opportunities for students from higher learning institutions.<br><br>
 							We foster collaboration and knowledge transfer through the detachment program with our clients across Africa and other parts of the world to improve the quality of humanitarian intervention within MSF operations.<br><br>
-						</p>
-					</div>
-					<div class="col-sm-6">
-						<div class="video-wrapper">
-							<img src="{{URL('/')}}/assets/img/homepage-slider/wh1.jpg" alt="" height="210">
-						</div>
-					</div> --}}
-					<div class="col-sm-6">
+						</p> --}}
 						<h3>About Our Web App</h3>
 						<p>
 							We hope you will enjoy using our App and find it useful. If you have suggestions feel free to contact us. We want to meet your needs and improve. That’s why we plan to add more features as we launch the second phase of our Web App. The main functionalities we already planned to have in the near future are:- <br>
@@ -41,6 +35,11 @@
 							• Supply news updates from all OC’s<br>
 							• Exploring ERP integration<br>
 						</p>
+					</div>
+					<div class="col-sm-6">
+						<div class="video-wrapper">
+							<img src="{{URL('/')}}/assets/img/homepage-slider/group.jpg" alt="" height="385">
+						</div>
 					</div>
 					<div class="col-sm-12">
 						<br>
