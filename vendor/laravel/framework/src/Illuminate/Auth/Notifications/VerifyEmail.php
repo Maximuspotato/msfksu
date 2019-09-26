f6e08f69b3d564c5dcbd57818047d9eb246e614f
Maximuspotato <rickylui28@gmail.com>
2019-09-26 14:56:53 +0300

    added vendor folder

---
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/VerifyEmail.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Illuminate\Auth\Notifications;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VerifyEmail extends Notification
+{
+    /**
+     * The callback that should be used to build the mail message.
+     *
+     * @var \Closure|null
+     */
+    public static $toMailCallback;
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
+        }
+
+        return (new MailMessage)
+            ->subject(Lang::getFromJson('Verify Email Address'))
+            ->from('ksucatalogue@gmail.com', 'MSF KSU')
+            ->bcc('MSFOCB-KSU-IT@brussels.msf.org')
+            ->line(Lang::getFromJson('Please wait while we verify your email address.'))
+            // ->action(Lang::getFromJson('Verify Email Address'), $verificationUrl)
+            ->line(Lang::getFromJson('If you we do not contact you within 24 hrs and you should be verified please contact us through our contacts page.'));
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            ['id' => $notifiable->getKey()]
+        );
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function toMailUsing($callback)
+    {
+        static::$toMailCallback = $callback;
+    }
+}
