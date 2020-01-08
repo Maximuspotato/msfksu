fd9624ab92ccb0a1fd4093cc7b9f69086cc0d857
Maximuspotato <rickylui28@gmail.com>
2020-01-08 11:53:30 +0300

    removed leaflet js

--- resources/views/incs/scripts.blade.php
+++ resources/views/incs/scripts.blade.php
@@ -2,7 +2,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
     <script src="{{URL('/')}}/assets/js/bootstrap.min.js"></script>
-    <script src="https://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
+    {{-- <script src="https://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script> --}}
     <script src="{{URL('/')}}/assets/js/jquery.fitvids.js"></script>
     <script src="{{URL('/')}}/assets/js/jquery.sequence-min.js"></script>
     <script src="{{URL('/')}}/assets/js/jquery.bxslider.js"></script>
