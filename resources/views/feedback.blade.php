aa12e37682f6d65e20be7819b2f63972f0721535
Maximuspotato <rickylui28@gmail.com>
2020-01-16 12:00:09 +0300

    edited feedback page layout

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -14,8 +14,42 @@
     
     <div class="section">
         <div class="container">
-            <div>
-                You can help us become better. Follow this <a href="https://docs.google.com/forms/d/e/1FAIpQLSepal27e56EyeAq3hM4Og_XPiSFr_hwiuIvnbTOFZJG2yv7hQ/viewform" target="_blank">link</a> and fill out the form provided.
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSepal27e56EyeAq3hM4Og_XPiSFr_hwiuIvnbTOFZJG2yv7hQ/viewform" target="_blank">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-xs-2">
+                                        <img src="{{URL('/')}}/assets/img/fb_wapp.png" alt="" height="80">
+                                    </div>
+                                    <div class="col-xs-10">
+                                        <h3 class="card-title">Webapp feedack form</h3>
+                                        <p class="card-text">With your input we would like to improve our WebApp</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </a>
+                </div>
+                <div class="col-md-6 disabled">
+                    <a href="">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-xs-2">
+                                        <img src="{{URL('/')}}/assets/img/fb_ops.png" alt="" height="80">
+                                    </div>
+                                    <div class="col-xs-10">
+                                        <h3 class="card-title">KSU Operations feedack form</h3>
+                                        <p class="card-text" style="margin-bottom:0">You can help KSU to become better by sharing your ideas, suggestions, complaints, claims and complements etc.</p>
+                                        <p style="color:red"><b>COMING SOON!</b></h4><p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
