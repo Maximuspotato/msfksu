ea8f20b48e5eb5d001e6396b5f895152043ee5d6
Maximuspotato <rickylui28@gmail.com>
2020-02-24 15:22:59 +0300

    resized 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -17,10 +17,10 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <img src="{{URL('/')}}/assets/img/404.jpg" alt="">
+                <img src="{{URL('/')}}/assets/img/404.jpg" alt="" height="300" class="center-block">
                 <br><br>
-                <h5>Seems like something went wrong or you encountered an error! Go back our Homepage by clicking the button below before you get SCRATCHED!!</h5>
-                <a class="btn" href="{{URL('/')}}">I've seen enough, please take me back to safety</a>
+                <h5 class="text-center">Seems like something went wrong or you encountered an error! Go back our Homepage by clicking the button below before you get SCRATCHED!!</h5>
+                <a class="btn center-block" href="{{URL('/')}}" style="display: block; width: 300px">I've seen enough, please take me back to safety</a>
             </div>
         </div>
     </div>
