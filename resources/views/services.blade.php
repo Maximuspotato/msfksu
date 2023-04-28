48dd3e7a692869d4cfe7efd8d532d30c13b0e1ed
MSFuser <rickylui28@gmail.com>
2023-04-28 15:36:14 +0300

    added content to services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -19,6 +19,31 @@
                     <div class="row service-wrapper-row">
                         <div class="col-sm-4">
                             <div class="service-image">
+                                <img src="{{URL('/')}}/assets/img/service-icon/popular.png" alt="Service Name" height="80">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <h3>Partial Order Confirmation: Alignment with ESC frame to treat orders</h3>
+                            <p>As you may know, the ESCs are sending partial order confirmation. This way, we would avoid losing too much time on confirming an entire order while only few lines are pending</p>
+                            <p>To make sure that you keep an entire overview of your initial request, you can access our Extranet (please contact us if you face any challenges or need some guidance for the first time) to the overview of the entire PO/RFQ</p>
+                        </div>
+                    </div>
+                    <div class="row service-wrapper-row">
+                        <div class="col-sm-4">
+                            <div class="service-image">
+                                <img src="{{URL('/')}}/assets/img/service-icon/popular.png" alt="Service Name" height="80">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <h3>Standard List</h3>
+                            <p>In order to be proactive, we are updating continuously KSU Standard List. This list is dynamic and you can access on the website and in Unicat</p>
+                            <p>You have a column with indication about pricing. Some items have a defined price (already negotiated with the supplier), some would hold only an indicative price (based on a purchase done in past 3 month) and some would have no price (meaning not purchased in past 3 months and no agreement with a supplier)</p>
+                            <p>For the items not in the Standard List, we welcome your request and will work on it to provide you feedback about our capacity (specification of the products)</p>
+                        </div>
+                    </div>
+                    <div class="row service-wrapper-row">
+                        <div class="col-sm-4">
+                            <div class="service-image">
                                 <img src="{{URL('/')}}/assets/img/service-icon/procurement.png" alt="Service Name" height="80">
                             </div>
                         </div>
@@ -57,6 +82,35 @@
                     </div>
                 </div>
                 <div class="col-sm-6">
+                    <div class="row service-wrapper-row">
+                        <div class="col-sm-4">
+                            <div class="service-image">
+                                <img src="{{URL('/')}}/assets/img/service-icon/popular.png" alt="Service Name" height="80">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <h3>Commitment on Lead-Time</h3>
+                            <p>For the items in the Standard List, we will send you an Order Confirmation (ORC) within 5 working days</p>
+                            <p>For the items not in the Standard list, we will send you a feedback (ORC with detailed specification) within 10 working days</p>
+                            <p>Please note that in the ORC, you will have information about price but also Lead-time to put the items in Ready To Ship zone</p>
+                            <br><br>
+                        </div>
+                    </div>
+                    <div class="row service-wrapper-row">
+                        <div class="col-sm-4">
+                            <div class="service-image">
+                                <img src="{{URL('/')}}/assets/img/service-icon/popular.png" alt="Service Name" height="80">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <h3>Order Confirmation validity</h3>
+                            <p>
+                                Considering the dynamic market in which we are working in, we need you to confirm the ORC as quick as possible. For this reason, we expect to have a validation of the ORC within 2 weeks from the day you receive the ORC
+                            </p>
+                            <p>We will send you a first reminder after one week. If no feedback after 10 days, we will send you a final reminder informing you that the order will be cancelled in the coming days</p>
+                            <p>In case, you are in the final process of validation and need more than 2 weeks, please inform us so we can try our best to maintain the condition mentioned in the ORC. Unfortunately, we can’t guarantee that we will be successful</p>
+                        </div>
+                    </div>
                     <div class="row service-wrapper-row">
                         <div class="col-sm-4">
                             <div class="service-image">
