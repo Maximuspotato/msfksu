7915b37f1c5a7c3fbc505c72767069c627ea5094
MSFuser <rickylui28@gmail.com>
2024-01-29 18:04:27 +0300

    added powerbitask 5 mins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('command:powerbitask')
-        //          ->everyFiveMinutes();
+        $schedule->command('command:powerbitask')
+                 ->everyFiveMinutes();
     }
 
     /**
