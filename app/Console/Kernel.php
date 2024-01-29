822629b2d1d1dbf026b158d5a7c5517ea318a1e7
MSFuser <rickylui28@gmail.com>
2024-01-29 19:25:20 +0300

    fixed powerbitask 5 mins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\PowerbiTask::class
     ];
 
     /**
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:powerbitask')
-                 ->everyMinute();
+                 ->everyFiveMinutes();
     }
 
     /**
